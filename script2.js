ebd0f8e6edc3fdc0516b3554be8a5a879b73c23f
Aryean Bhojak <aryeanbhojak@gmail.com>
2025-03-31 18:14:35 +0530

    weather

--- script2.js
+++ script2.js
@@ -1,178 +1,43 @@
-const canvas=document.getElementById('canvas1');
-const ctx=canvas.getContext('2d');
-canvas.width=1150;
-canvas.height=500;
+const userLocation=document.getElementById("userLocation"),converter=document.getElementById("converter"),weatherIcon=document.querySelector(".weatherIcon"),temperature=document.querySelector(".temperature"),feelslike=document.querySelector(".feelslike"),description=document.querySelector(".description"),date=document.querySelector(".date"),city=document.querySelector(".city"),hValue=document.getElementById("hValue"),wValue=document.getElementById("wValue"),sRValue=document.getElementById("sRValue"),sSValue=document.getElementById("sSValue"),cValue=document.getElementById("cValue"),uVValue=document.getElementById("uVValue"),pValue=document.getElementById("pValue"),Forecast=document.querySelector(".Forecast");
 
-var lineW=9;
-ctx.lineWidth=lineW;
-let prevX=null;
-let prevY=null;
-let draw=false;
+WEATHER_API_ENDPOINT=`https://api.openweathermap.org/data/2.5/weather?appid=c3fab1d8f40717b8a9a3b2abeae91ac4&q=`;
+WEATHER_DATA_ENDPOINT=`https://api.openweathermap.org/data/2.5/onecall?appid=c3fab1d8f40717b8a9a3b2abeae91ac4&exclude=minutely&units=metric&`;
 
+function findUserLocation(){
+	fetch(WEATHER_API_ENDPOINT+ userLocation.value)
+	.then((response)=>response.json())
+	.then((data)=>{
+		if(data.cod!=''&& data.cod!=200){
+			alert(data.message);
+			return;
+		}
+		console.log(data);
 
-const high=document.getElementsByClassName("high")[0];
-const adtxt=document.getElementsByClassName("adtxt")[0];
-const sve=document.getElementsByClassName("sve")[0];
+		city.innerHTML=data.name+", "+data.sys.country;
+		temperature.innerHTML=data.main.temp;
+		feelslike.innerHTML="Feels Like"+data.main.feels_like;
+		description.innerHTML=`<i class="fa-brands fa-cloudversify"></i> &nbsp`+data.weather[0].description;
+		hValue.innerHTML=data.main.humidity+"%";
+		wValue.innerHTML=data.wind.speed+"ms";
 
-const ersr=document.getElementsByClassName("ersr")[0];
-const clre=document.getElementsByClassName("clre")[0];
-high.style.background="red";
-ersr.style.background="red";
-high.addEventListener("click",function(){
-	if(high.style.background==="red"){
-		high.style.background="green";
-	}else{
-		high.style.background="red";
-	}
-});
-
-
-
-
-adtxt.addEventListener("click",function(){
-	adtxt.style.background="green"
-	var a=prompt("Enter text to be inserted");
-	ctx.lineWidth=lineW;
-	ctx.font=`${document.getElementById('ageInputId').value * 4}px Arial`;
-	console.log(a);
-	window.addEventListener("mousemove",(e)=>{
-	if(prevX===null||prevY===null||!draw){
-		prevX=e.offsetX;
-		prevY=e.offsetY+25;
-		return}
-
-	else if(adtxt.style.background==="green"){
-		ctx.fillStyle=conColor.value;
-		ctx.fillText(a,prevX,prevY);
-		adtxt.style.background="red"
-	}
-})
-});
-
-
-	
-
-
-
-
-sve.addEventListener("click",function(){
-	let data=canvas.toDataURL("image/png");
-	let a=document.createElement("a");
-	a.href=data;
-	a.download="sketch.png";
-	a.click();
-});
-
-
-clre.addEventListener("click",function(){
-	ctx.clearRect(0, 0, canvas.width, canvas.height);
-});
-
-
-var bgcolor1=document.querySelector(".bgcolor1");
-var bgcolor2=document.querySelector(".bgcolor2");
-function setGradient(){
-	canvas.style.background="linear-gradient(to right, "
-	+bgcolor1.value
-	+", "
-	+bgcolor2.value
-	+")";
-}
-bgcolor1.addEventListener("input",setGradient);
-bgcolor2.addEventListener("input",setGradient);
-
-const conColor=document.querySelector(".conColor");
-function setConColor(){
-	ctx.strokeStyle=conColor.value
-}
-conColor.addEventListener("input",setConColor);
-
-
-
-
-window.addEventListener("mousedown",(e) => draw=true);
-window.addEventListener("mouseup",(e) => draw=false);
-
-document.getElementById("ageInputId").oninput=function(){
-	draw=null;
-	lineW=document.getElementById("ageInputId").value;
-	document.getElementById("ageOutputId").innerHTML=lineW;
-	ctx.lineWidth=lineW;
-	
-}	
-
-window.addEventListener("mousemove",(e)=>{
-	if(prevX===null||prevY===null||!draw){
-		prevX=e.offsetX;
-		prevY=e.offsetY+25;
-		return}
-
-	
-	
-	else if(high.style.background==="red" && ersr.style.background==="red"){
-
-		ctx.globalAlpha=1;
-		const currentX=e.offsetX;
-		const currentY=e.offsetY+25;
+		sSValue.innerHTML=data.main.temp_min;
+		cValue.innerHTML=data.clouds.all+"%";
+		uVValue.innerHTML=data.main.temp_max;
+		pValue.innerHTML=data.main.pressure+"hPa";
 
 		
+		weatherIcon.style.background='url(https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png)'
+		fetch(
+			WEATHER_DATA_ENDPOINT+`lon=$(data.coord.lon)&lat=$(data.coord.lat)`)
+			.then((response)=>response.json())
+			.then((data)=>{
+				console.log(data);
 
-		ctx.beginPath();
-		ctx.moveTo(prevX,prevY);
-		ctx.lineTo(currentX,currentY)
-		ctx.stroke();
-		prevX=currentX;
-		prevY=currentY;
-
-	}
-	else if(high.style.background==="green" && ersr.style.background==="red"){
-
-		let currentX=e.offsetX;
-		let currentY=e.offsetY+25;
-		ctx.globalAlpha=0.3;
-
-
-		ctx.beginPath();
-		ctx.moveTo(prevX,prevY);
-		ctx.lineTo(currentX,currentY)
-		ctx.stroke();
-
-		prevX=currentX;
-		prevY=currentY;
-	}
-	else if(ersr.style.background==="green"){
-		ctx.globalAlpha=1;
-		let currentX=e.offsetX;
-		let currentY=e.offsetY+25;
-
-		ctx.beginPath();
-		ctx.moveTo(prevX,prevY);
-		ctx.clearRect(currentX,currentY,10,10);
-		ctx.stroke();
-
-		prevX=currentX;
-		prevY=currentY;
-	}
-})
-
-
-
-
-
-
-ersr.addEventListener("click",function(){
-	if(ersr.style.background==="red"){
-		ersr.style.background="green";
-	}else{
-		ersr.style.background="red";
-	}
-});
-
-
-
-
-
-	
-
-
-
+				
+				
+				
+				
+			});
+		
+	});
+}
